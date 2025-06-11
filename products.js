0f2146d5886a759eb0ca85986debdbcbee6e024a
dbarreiro19 <dbarreiro88@outlook.com>
2025-06-10 21:20:49 -0300

    Subindo arquivos do projeto

---
+++ products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    {name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpg'},
+    {name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.jpg'},
+    {name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.jpg'},
+    {name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.jpg'},
+    {name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.jpg'},
+    {name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.jpg'}
+]
